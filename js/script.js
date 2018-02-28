e92bb270e692730afe99c36151437cc2c9c2f29c
Mauro Quinteros Prosperi <mauro.q.prosperi@gmail.com>
2018-02-28 00:39:04 -0300

    javascript added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+	var map;
+	function initMap(){
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: -32.912951, lng: -68.862329},
+			zoom: 13
+		});
+
+ 	var gordoHome = {lat: -32.946843,lng: -68.804855};
+ 	var marker = new google.maps.Marker({
+ 		position: gordoHome,
+ 		map: map,
+ 		title: ' First Marker'
+ 	});
+}
+ initMap();
